cd407fda1b2e59a02095cb538c9768204f6e2e98
Leruiz-01 <142291530+Leruiz-01@users.noreply.github.com>
2024-02-08 19:52:26 -0600

    Delete Propuesta sitio web directory

--- Propuesta sitio web/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('contactForm'),addEventListener('submit', function(event){
-    event.preventDefault();
-    alert('Has dado tu primer paso a tu Santurio Zen. Espera un mensaje pronto de nosotros, ¡un futuro relajante te espera!');
-
-}
-
-
-
-);
