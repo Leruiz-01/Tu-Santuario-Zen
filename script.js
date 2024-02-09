1cd0ef497f390f7f1bf7a895e5f392907afde1eb
Leruiz-01 <142291530+Leruiz-01@users.noreply.github.com>
2024-02-08 21:24:39 -0600

    Subir datos
    Subir datos

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('contactForm'),addEventListener('submit', function(event){
+    event.preventDefault();
+    alert('Has dado tu primer paso a tu Santurio Zen. Espera un mensaje pronto de nosotros, ¡un futuro relajante te espera!');
+
+}
+
+
+
+);
